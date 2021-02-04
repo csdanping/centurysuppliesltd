e55ec13835e6423bd40d166cf8ee3f175ccd61b0
zhongdp <zhongdanping@rhtect.com>
2021-02-04 10:12:36 +0800

    init

---
+++ menumachine/top_nav/menuspecs.js
@@ -0,0 +1,17 @@
+/*
+MenuMachine 2 definition file - do not edit. http://menumachine.com
+2.2.1 :: top_nav
+*/
+var menuName="top_nav";
+var pkg=new menuPackage(menuName,0,0,0,0,0,1,0,0,1);
+/*s*/
+pkg.aB("m4ac0396","",32.0625,3.875,0,0,0,0,0,0,0,0,0,"h",100,"",0,0,"",0,"black",0,0,"black",0,0,0,"","","","",0,0,0,0,0,0,0,0,0);
+pkg.aI("meb56a07","m4ac0396",6.5,3.875,0,0,"","",/*URL*/"../../index.html","",/*URL*/"../../homepage/home1.gif",/*URL*/"../../homepage/home2.gif",/*URL*/"../../homepage/home1.gif",0,0,0,0,"","",0,0,"Verdana,Arial,Helvetica,sans-serif","#AAAAAA","black",0.6875,0,0,0,"black",0,1,0,0,0.3125,"",0,0);
+pkg.aI("m2xvqute","m4ac0396",5.0625,3.875,6.5,0,"","",/*URL*/"../../services.html","",/*URL*/"../../homepage/services1.gif",/*URL*/"../../homepage/services2.gif",/*URL*/"../../homepage/services1.gif",0,0,0,0,"","",1,1);
+pkg.aI("m2xvqutf","m4ac0396",7.375,3.875,11.5625,0,"","",/*URL*/"../../companyinfo.html","",/*URL*/"../../homepage/companyinfo1.gif",/*URL*/"../../homepage/companyinfo2.gif",/*URL*/"../../homepage/companyinfo1.gif",0,0,0,0,"","",2,1);
+pkg.aI("m2xvqutg","m4ac0396",5.875,3.875,18.9375,0,"","",/*URL*/"../../products.html","",/*URL*/"../../homepage/products1.gif",/*URL*/"../../homepage/products2.gif",/*URL*/"../../homepage/products1.gif",0,0,0,0,"","",3,1);
+pkg.aI("m2xvquth","m4ac0396",7.25,3.875,24.8125,0,"","",/*URL*/"../../contactus.html","",/*URL*/"../../homepage/contactus1.gif",/*URL*/"../../homepage/contactus2.gif",/*URL*/"../../homepage/contactus1.gif",0,0,0,0,"","",4,1);
+/*f*/
+__menuHolder.aP(pkg);
+var __l=__br.un?"un.js":(__br.ie?"iedom.js":(__br.dom?"w3cdom.js":""));
+document.write(_sTs+mmfolder+"core/"+__l+_sTe);
